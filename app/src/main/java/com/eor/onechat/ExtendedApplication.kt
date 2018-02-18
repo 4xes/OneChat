f4d6969d91ff319bd6890d9d4193ee5a708ccf8b
Salnikov Sergey <ncr98@yandex.ru>
2018-02-18 04:33:06 +0300

    fix deviceId

--- app/src/main/java/com/eor/onechat/ExtendedApplication.kt
+++ app/src/main/java/com/eor/onechat/ExtendedApplication.kt
@@ -1,6 +1,7 @@
 package com.eor.onechat
 
 import android.app.Application
+import android.provider.Settings
 import com.eor.onechat.net.Auth
 import com.eor.onechat.net.Proto
 import com.eor.onechat.net.ServerResponse
@@ -16,13 +17,17 @@ import timber.log.Timber
 class ExtendedApplication: Application() {
 
     private var webSocketClient: WebSocketClient? = null
-    private val authReq: Auth = Auth("sselgq66-1426-11e8-b642-0ed5f89f718b", "Serg Salnikov");
+
     var auth: Auth? = null
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         Timber.v("onCreate")
+
+        val androidId = Settings.Secure.getString(this.contentResolver, Settings.Secure.ANDROID_ID)
+        val authReq = Auth(androidId, "Robot Androidovich");
+
         webSocketClient = WebSocketClient()
 
         val authResponse = object: ServerResponse {
